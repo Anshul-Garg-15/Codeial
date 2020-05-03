e13af2c6ffdc53bedde71949208f8bb7dc1bd9c4
AnshulGarg <anshulgargg06@gmail.com>
2020-05-03 21:26:49 +0530

    Set up view engine

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,4 +1,8 @@
 module.exports.home = function(request , respond){
-    return respond.end('<h1>Express Server is Running</h1>');
+    // return respond.end('<h1>Express Server is Running</h1>');
+
+    return respond.render('home',{
+        title: "Home"
+    });
 
 }
