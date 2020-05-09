17185b1774556e28710fc29748bd80810e965471
AnshulGarg <anshulgargg06@gmail.com>
2020-05-09 17:05:05 +0530

    Set up Delete post and comment part with authentication and authorisation

--- routes/comment.js
+++ routes/comment.js
@@ -6,6 +6,6 @@ const commentController = require('../controllers/commentController');
 
 //able to create the post only if user signed in
 router.post('/create' ,passport.checkAuthentication, commentController.create)
-
+router.get('/destroy/:id' , passport.checkAuthentication,commentController.destroy);
 
 module.exports = router;
